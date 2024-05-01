3ff0b42c93f1634b7205658a6ddb29ec8b2f49f7
Neha-Bhatia551 <nbhatia4@depaul.edu>
2024-05-01 12:55:35 -0500

    Initial setup of spring repo

---
+++ src/main/java/com/depaul/edu/portfoliocraft/PortfoliocraftApplication.java
@@ -0,0 +1,13 @@
+package com.depaul.edu.portfoliocraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfoliocraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfoliocraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depaul/edu/portfoliocraft/PortfoliocraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depaul.edu.portfoliocraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliocraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
