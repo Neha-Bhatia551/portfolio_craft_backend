495397f12fcf744988a3f930463b4f986a4a38a3
Neha-Bhatia551 <nbhatia4@depaul.edu>
2024-05-09 22:36:08 -0500

    PORTFOLIO-18 Add database connection

--- src/test/java/com/depaul/edu/portfoliocraft/PortfoliocraftApplicationTests.java
+++ src/test/java/com/depaul/edu/portfoliocraft/PortfoliocraftApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PortfoliocraftApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
